dd8e2a89813249554804757cd1c4e832294d38a9
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-20 08:51:55 +0530

    Delete mod.js

--- mod.js
+++
@@ -1,5 +0,0 @@
-function changecolor(){
-    var randomcolor=Math.floor(Math.random()*16777215).toString(20);
-    document.body.style.backgroundColor='#'+randomcolor;
-}
-setInterval(changecolor,300);
